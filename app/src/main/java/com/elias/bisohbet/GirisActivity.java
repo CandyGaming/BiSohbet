fdc472fc1b5def9fcf4c63b9370367e8fa347442
DilaraCeylan <dilaraaaceylannn@gmail.com>
2018-08-14 09:31:11 +0300

    Yeniyiz2

--- app/src/main/java/com/elias/bisohbet/GirisActivity.java
+++ app/src/main/java/com/elias/bisohbet/GirisActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class GirisActivity extends AppCompatActivity {
-    int AblaaBu;
+    int AblaaBus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
