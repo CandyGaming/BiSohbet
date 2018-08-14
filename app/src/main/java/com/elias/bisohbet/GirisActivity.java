77ad62d7d328e85abd5d666d7ff70fbd496d31ce
Ömer Sina Ceylan <portakaorda44@gmail.com>
2018-08-14 08:53:44 +0300

    Test2

--- app/src/main/java/com/elias/bisohbet/GirisActivity.java
+++ app/src/main/java/com/elias/bisohbet/GirisActivity.java
@@ -4,11 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class GirisActivity extends AppCompatActivity {
-
+    int Abla;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giris);
         //Ablaa
+
     }
 }
